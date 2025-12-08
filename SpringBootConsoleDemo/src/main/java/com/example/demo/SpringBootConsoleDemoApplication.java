1d7344e8319e6dab09fe9b9cc507ac1df527e540
Deepak <deepakkumar62559@gmail.com>
2025-12-08 17:54:51 +0530

    Start with spring boot

---
+++ SpringBootConsoleDemo/src/main/java/com/example/demo/SpringBootConsoleDemoApplication.java
@@ -0,0 +1,14 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootConsoleDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootConsoleDemoApplication.class, args);
+		System.out.println("welcome to springboot project");
+	}
+
+}
